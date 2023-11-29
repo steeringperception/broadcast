9be33606c39c4b4c23dd850b2b9f3d5d76e39d18
Steering Perception <mail2anuragmishra@gmail.com>
2023-11-29 09:52:41 +0530

    d

--- public/js/umami.js
+++ public/js/umami.js
@@ -2,8 +2,8 @@
 
 // https://github.com/mikecao/umami
 
-const script = document.createElement('script');
-script.setAttribute('async', '');
-script.setAttribute('src', 'https://stats.mirotalk.com/script.js');
-script.setAttribute('data-website-id', '4250d19f-a91a-4b75-bac5-7d19961b329e');
-document.head.appendChild(script);
+// const script = document.createElement('script');
+// script.setAttribute('async', '');
+// script.setAttribute('src', 'https://stats.mirotalk.com/script.js');
+// script.setAttribute('data-website-id', '4250d19f-a91a-4b75-bac5-7d19961b329e');
+// document.head.appendChild(script);
